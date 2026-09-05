900b42f593d9be7003d94ddbd47e36a2e19091d9
Gian <gian94794@gmail.com>
2026-09-05 19:28:35 +0800

    Add switch statement to determine season

---
+++ switch.java
@@ -0,0 +1,26 @@
+class oop {
+   public static void main(String[] args){
+  int season = 4;
+  switch (season) {
+    case 1:
+        System.out.print("summer");
+        break;
+    case 2:
+        System.out.print("Spring");
+        break;
+    case 3:
+        System.out.print("Autumn");
+        break;
+    case 4:
+        System.out.print("Winter");
+        break;
+    default:
+        System.out.print("No defined season.");
+        break;
+  }
+    }
+}
+
+
+      
+    
