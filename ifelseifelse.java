babc9843a63afdfc5433a27c4987e39716ab8609
Gian <gian94794@gmail.com>
2026-09-05 19:01:48 +0800

    Add grade evaluation using if-else -if statements

---
+++ ifelseifelse.java
@@ -0,0 +1,18 @@
+class oop {
+   public static void main(String[] args){
+    int grade = 90;
+
+    if(grade >= 90){
+        System.out.print("Grade: A");
+    }
+    else if(grade >= 85){
+        System.out.print("Grade: B");
+    }
+    else if(grade >= 75){
+        System.out.print("Grade C");
+    }
+    else{
+        System.out.print("You failed!");
+    }
+   }
+}
